41588d31e722dbf5fd171f9064c94aca895c3597
Adam Nunez <adam.b.nunez@gmail.com>
2015-03-28 13:58:01 -0700

    hello dicks

--- 360Phase2Source/Main.java
+++ 360Phase2Source/Main.java
@@ -1,6 +1,7 @@
 //dicks new stuff
+//newstuff 2
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, dicks!");
     }
 }
