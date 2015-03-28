a5d1d539016d26f60cf16e57dff875a4a4420e67
Adam Nunez <adam.b.nunez@gmail.com>
2015-03-28 15:42:47 -0700

    Doc1

---
+++ src/Doctor.java
@@ -0,0 +1,23 @@
+public class Doctor
+{
+   
+   private Patient [] patientList;
+
+   public Doctor(Patient[] patientL)
+   {
+	   super();
+	   
+	   this.patientList = patientL;
+   }
+   
+   public getPatientList()
+   {
+	   return this.patientList;
+   }
+   
+   public setPatientList(Patient[] patientL)
+   {
+	   this.patientList = patientL;
+   }
+  
+}
